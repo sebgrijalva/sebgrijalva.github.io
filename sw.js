7a70be819f539f671318bd4cf250f959376bae59
Seb Grijalva <13460713+sebgrijalva@users.noreply.github.com>
2026-07-25 19:58:38 -0700

    Cache v16 platformer build

--- sw.js
+++ sw.js
@@ -1,39 +1,32 @@
-const CACHE = "family-adventure-v15-matter";
+const CACHE = "family-adventure-v16";
 const ASSETS = [
-  "./?v=15",
+  "./?v=16",
   "./index.html",
-  "./styles.css?v=15",
-  "./app.js?v=15",
+  "./styles.css?v=16",
+  "./app.js?v=16",
   "./engine.js?v=15",
   "./adventure.js?v=15",
   "./adventure-v14.js?v=15",
   "./matter-adventure.js?v=15",
-  "./vendor/matter.min.js?v=15",
+  "./platformer-v16.js?v=16",
+  "./vendor/matter.min.js?v=16",
   "./manifest.webmanifest",
-  "./icons/icon.svg"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
